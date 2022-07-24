968851424eb2e73524bb57bbf8d76de90fb53d42
Laura nascente <92733398+itsmelaura07@users.noreply.github.com>
2022-07-23 21:53:39 -0300

    Atualização consulta

--- Sitesigma-Laura/Consulta.php
+++ Sitesigma-Laura/Consulta.php
@@ -1,1 +1,16 @@
+<?php
+// include do footer
+include_once './includes/_dados.php';
+include_once './includes/_head.php';
+include_once './includes/_header.php';
+include_once './includes/banco.php';
+?>
 
+
+
+<h1>Consulta</h1>
+
+<?php
+// include do footer
+include_once './includes/_footer.php';
+?>
