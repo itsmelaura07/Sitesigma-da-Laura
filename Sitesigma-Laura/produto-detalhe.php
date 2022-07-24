4db8380eace44f4426bdfac16ba921c81bfb3b33
Laura nascente <92733398+itsmelaura07@users.noreply.github.com>
2022-07-23 21:50:23 -0300

    Atualização produto detalhe

--- Sitesigma-Laura/produto-detalhe.php
+++ Sitesigma-Laura/produto-detalhe.php
@@ -5,18 +5,19 @@ include_once './includes/_head.php';
 include_once './includes/_header.php';
 include_once './includes/_banco.php';
 
-
-
-$id = $_GET['id'];
-echo $id;
 ?>
 
+
+
+<div  class="prod">
+<h2 id='titulo' class="clear">Produtos</h2>
+    <div class="row mt-5">
 
 <div class="center">
     <div class="row">
         <div class="col">
 <h1><?php echo $produtos [$id] ['nome'];?></h1>
-<img class="img"src="./content/<?php echo $dados[$id]['imagem'];?>">
+<img class="img"src="./content/<?php echo $produtos[$id]['imagem'];?>">
 <h3><?php echo $produtos [$id] ['descricao'];?></h3>
 <h3><?php echo $produtos [$id] ['preco'];?></h3>
 </div>
